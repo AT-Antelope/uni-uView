fc2c896503667ae3051ae3cc2272aec9a809a535
AT <q263629656@gmail.com>
2021-12-30 10:39:52 +0800

    初始化项目

---
+++ components/tabbar/tabbar.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<u-tabbar :value="tabbarIndex" :fixed="false" :placeholder="false" :safeAreaInsetBottom="false">
+			<u-tabbar-item text="首页" icon="home" @click="btnTabbar"></u-tabbar-item>
+			<u-tabbar-item text="我的" icon="account" @click="btnTabbar"></u-tabbar-item>
+		</u-tabbar>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'tabbar',
+	data() {
+		return {
+			tabbarIndex: 0
+		};
+	},
+	methods: {
+		btnTabbar(e) {
+			this.tabbarIndex = e;
+		}
+	}
+};
+</script>
+
+<style></style>
